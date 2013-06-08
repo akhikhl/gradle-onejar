86181a130ef73812124122193c52e7ea37f3ef32
Andrey Hihlovskiy <akhikhl@gmail.com>
2013-06-08 18:38:43 +0200

    optimizations in task initialization, introduced flavor properties.

--- libs/gradle-onejar/src/main/groovy/onejar/OneJarPlugin.groovy
+++ libs/gradle-onejar/src/main/groovy/onejar/OneJarPlugin.groovy
@@ -10,7 +10,7 @@ class OneJarPlugin implements Plugin<Project> {
 
     // the project supposed to be "java" or "groovy" already
 
-    def onejarExt = project.ext.has("onejar") ? project.ext.onejar : []
+    def onejarExt = project.ext.properties.onejar ?: []
 
     if(onejarExt.mainClass)
       project.jar {
@@ -19,27 +19,39 @@ class OneJarPlugin implements Plugin<Project> {
     else
       project.logger.warn("Main class is not specified")
 
-    def flavors = onejarExt.flavors ? onejarExt.flavors : ["default"]
+    def flavors = onejarExt.flavors ?: ["default"]
+
+    def findFileInFlavors = { file ->
+      flavors.find { flavor ->
+        flavor = flavor instanceof String ? [ name: flavor ] : flavor
+        project.configurations.findByName(flavor.name)?.find { it == file }
+      } }
 
     project.configurations { onejar }
 
     project.dependencies { onejar "com.simontuffs:one-jar-ant-task:0.97" }
 
     flavors.each { flavor ->
+      flavor = flavor instanceof String ? [ name: flavor ] : flavor
+
       def buildTaskName = "oneJarBuild"
-      if(flavor != "default")
-        buildTaskName += "_" + flavor
-      project.task(buildTaskName) {
-        inputs.dir "${project.buildDir}/libs"
+      if(flavor.name != "default")
+        buildTaskName += "_" + flavor.name
+
+      project.task(buildTaskName) { task ->
+
         def outputDir = "${project.buildDir}/output"
-        if(flavor != "default")
-          outputDir += "-" + flavor
+        if(flavor.name != "default")
+          outputDir += "-" + flavor.name
+
+        inputs.dir "${project.buildDir}/libs"
         outputs.dir outputDir
+
         doLast {
           ant.taskdef(name: 'onejar', classname: "com.simontuffs.onejar.ant.OneJarTask", classpath: project.configurations.onejar.asPath)
           def baseName = "${project.name}-${project.version}"
           if(flavor != "default")
-            baseName += "-" + flavor
+            baseName += "-" + flavor.name
           def destFile = "${outputDir}/${baseName}.jar"
           ant.onejar(destFile: destFile) {
             main(jar: project.tasks.jar.archivePath.toString())
@@ -52,30 +64,30 @@ class OneJarPlugin implements Plugin<Project> {
                 attribute(name: "Built-By", value: System.getProperty("user.name"))
             }
             lib {
-              project.configurations.runtime.each { File file ->
-                boolean foundInFlavors = flavors.find { flavor2 ->
-                  return project.configurations.findByName(flavor2) && project.configurations[flavor2].find { it == file }
-                }
-                if(!foundInFlavors)
+              project.configurations.runtime.each { file ->
+                if(!findFileInFlavors(file))
                   fileset(file: file)
               }
-              if(project.configurations.findByName(flavor))
-                project.configurations[flavor].each { File file ->
-                  fileset(file: file)
-                }
+              project.configurations.findByName(flavor.name)?.each { file ->
+                fileset(file: file)
+              }
             }
           }
-          File launchScriptFile = new File("${outputDir}/${baseName}.sh")
-          launchScriptFile.text = "#!/bin/bash\njava -jar ${baseName}.jar \"\$@\""
-          launchScriptFile.setExecutable(true)
-          launchScriptFile = new File("${outputDir}/${baseName}.bat")
-          launchScriptFile.text = "@java -jar ${baseName}.jar %*"
+          if(flavor.launcher == "windows") {
+            def launchScriptFile = new File("${outputDir}/${baseName}.bat")
+            launchScriptFile.text = "@java -jar ${baseName}.jar %*"
+          } else {
+            def launchScriptFile = new File("${outputDir}/${baseName}.sh")
+            launchScriptFile.text = "#!/bin/bash\njava -jar ${baseName}.jar \"\$@\""
+            launchScriptFile.setExecutable(true)
+          }
           project.logger.info "Created one-jar: " + destFile
-        }
-      }
-      project.tasks[buildTaskName].dependsOn "assemble", "check"
+        } // doLast
 
-      project.tasks.build.dependsOn buildTaskName
+        dependsOn "assemble", "check"
+
+        project.tasks.build.dependsOn task
+      }
     }
   }
 }
