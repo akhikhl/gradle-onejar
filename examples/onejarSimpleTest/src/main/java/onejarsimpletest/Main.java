18559c766a22b33f0366d92aeed2c578d562e716
Andrey Hihlovskiy <ahi@startext.de>
2013-06-08 11:59:14 +0200

    onejasSimpleTest: added commons-lang3 library to demonstrate dependency integration

--- examples/onejarSimpleTest/src/main/java/onejarsimpletest/Main.java
+++ examples/onejarSimpleTest/src/main/java/onejarsimpletest/Main.java
@@ -1,8 +1,10 @@
 package onejarsimpletest;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, onejar!");
+    System.out.println("Hello, onejar" + StringUtils.repeat("!", 4));
   }
 }
